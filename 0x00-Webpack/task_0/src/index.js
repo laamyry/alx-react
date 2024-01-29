f8254ba59f5c3f6b06102ea3fb6f893b6ceb3823
laamyry <slamiri10@gmail.com>
2024-01-29 22:29:49 +0100

    add task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+import jquery from "jquery";
+
+$(document).ready(function () {
+  $("body").append("<p>Paragraph 1</p>");
+  $("body").append("<p>Paragraph 2</p>");
+  $("body").append("<p>Paragraph 3</p>");
+});
