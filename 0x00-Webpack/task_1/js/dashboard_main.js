289efc01ccc225b2c970898bbc3876fd1e7709fc
laamyry <slamiri10@gmail.com>
2024-01-31 01:57:58 +0100

    update task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,10 +2,10 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>'),
-$('body').append('<p>Dashboard data for the students</p>'),
-$('body').append('<button>Click here to get started</button>'),
-$('body').append("<p id='count'></p>"),
-$('body').append('<p>Copyright - Holberton School</p>');
+  $('body').append('<p>Dashboard data for the students</p>'),
+  $('body').append('<button>Click here to get started</button>'),
+  $('body').append("<p id='count'></p>"),
+  $('body').append('<p>Copyright - Holberton School</p>');
 
 const updateCounter = () => {
   let counter = $('#count').html() || 0;
--- 0x00-Webpack/task_1/src/index.js
+++
@@ -1,6 +0,0 @@
-import $ from "jquery"
-
-$('body').append('<p>Holberton Dashboard</P>'),
-$('body').append('<p>Dashboard data for the students</P>'),
-$('body').append('<p>Holberton Dashboard</P>'),
-$('body').append('<p>Holberton Dashboard</P>')
