ac9ad9bc12a0e34e27cd788ca303f4c3ce6c4bd0
laamyry <slamiri10@gmail.com>
2024-02-01 00:45:11 +0100

    add task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import logo from './logo.jpg';
+function App() {
+  return (
+    <body>
+      <div className='App-header'>
+        <img src={logo} />
+        <h1>School dashboard</h1>
+      </div>
+      <hr />
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className='App-footer'>
+        <hr />
+        <p>Copyright 2020 - holberton</p>
+      </div>
+    </body>
+  );
+}
+export default App;
