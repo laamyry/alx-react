c9636f8291633f07b13c19afdf50c7c1db11f36c
laamyry <slamiri10@gmail.com>
2024-02-03 14:24:29 +0100

    update task 3

--- 0x00-Webpack/task_3/modules/body/body.js
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -1,18 +1,17 @@
-'use strict';
-const $ = require('jquery');
-const _ = require('lodash');
-import './body.css';
+import "./body.css";
+const $ = require("jquery");
+const _ = require("lodash");
 
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"><p>');
 
-function updateCounter() {
-  let clicks = $('#count').html() || 0;
-  $('button').on('click', () => {
-    clicks++;
-    $('#count').html(`${clicks} clicks on the button`);
+const updateCounter = () => {
+  let times = $("#count").html() || 0;
+  $("button").on("click", () => {
+    times++;
+    $("#count").html(`${times} clicks on the button`);
   });
-}
+};
 
-_.debounce(updateCounter(), 500);
+_.debounce(updateCounter, 500);
+updateCounter();
--- 0x00-Webpack/task_3/modules/header/header.js
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -1,8 +1,7 @@
-'use strict';
-const $ = require('jquery');
-import './header.css';
+import "./header.css";
+const $ = require("jquery");
 
-$('body').append('<header></header>');
-$('header').append('<div id="logo"></div>', '<h1>Holberton Dashboard</h1>');
+$("body").append("<header></header>");
+$("header").append('<div id="logo"></div>', "<h1>Holberton Dashboard</h1>");
 
-console.log('Init header');
+console.log("Init header");
