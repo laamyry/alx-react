4c31407e29b30cd038d28b938de6b6ffbda339b4
laamyry <slamiri10@gmail.com>
2024-03-03 23:30:42 +0100

    add task 1

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Notification from "../Notifications/Notifications";
 import Login from "../Login/Login";
 import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils/utils";
 import proptypes from "prop-types";
+
 class App extends React.Component {
-  static listOfCourses = [
+  static listCourses = [
     { id: 1, name: "ES6", credit: 60 },
     { id: 2, name: "Webpack", credit: 20 },
     { id: 3, name: "React", credit: 40 },
   ];
+
   static listNotifications = [
     { id: 1, value: "New course available", type: "default" },
     { id: 2, value: "New resume available", type: "urgent" },
     { id: 3, html: { __html: getLatestNotification() }, type: "urgent" },
   ];
+
   constructor(props) {
     super(props);
     this.isLoggedIn = props.isLoggedIn;
     this.logOut = props.logOut;
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
+
   handleKeyDown(e) {
     e.preventDefault();
     if (e.ctrlKey && e.key === "h") {
@@ -38,20 +43,21 @@ class App extends React.Component {
   componentWillUnmount() {
     window.removeEventListener("keydown", this.handleKeyDown);
   }
+
   render() {
-    const { isLoggedIn } = this.props;
     return (
       <React.Fragment>
-        <Notifications listNotifications={this.listNotifications} />
+        <Notification listNotifications={this.listNotifications} />
         <div className="App">
           <Header />
-          {this.props.isLoggedIn ? <CourseList listOfCourses={this.listOfCourses} /> : <Login />}
+          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
           <Footer />
         </div>
       </React.Fragment>
     );
   }
 }
+
 App.defaultProps = {
   isLoggedIn: false,
   logOut: () => {},
@@ -59,6 +65,7 @@ App.defaultProps = {
 
 App.propTypes = {
   isLoggedIn: proptypes.bool,
-  logOut: PropTypes.func,
+  logOut: proptypes.func,
 };
+
 export default App;
