bcd108c5b615aff4de4621b19a9b236b74776ec1
laamyry <slamiri10@gmail.com>
2024-03-05 23:47:43 +0100

    add task 5

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import Notifications from './Notifications/Notifications';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<div className='root-notifications'>
+			<Notifications />
+		</div>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
